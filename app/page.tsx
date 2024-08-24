ddb691badc7126be869e06d81e2d4a8a1e76987d
cristiancutizaca <cutizacamachacacristian@gmail.com>
2024-08-24 18:03:54 -0500

    PrincipalHome-narBar-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
 const PrincipalHome = dynamic(() => import("@/components/home/PrincipalHome"));
-const PorqueNosotros = dynamic(
-  () => import("@/components/home/PorqueNosotros")
-);
 const SwiperCarrouselDiplomados = dynamic(
   () => import("@/components/home/SwiperDiplomados")
 );
@@ -51,7 +48,7 @@ export default function Main() {
       <div className="absolute w-[500px] h-96 blur-2xl -z-10 bg-gradient-radial dark:from-cyan-950 from-blue-100 to-transparent top-[60%] left-1/2 transform -translate-x-1/2 hidden md:block"></div>
       <div className="absolute w-[500px] h-96 blur-2xl -z-10 bg-gradient-radial dark:from-cyan-950 from-violet-100 to-transparent top-[60%] left-[40%] transform -translate-x-1/2 hidden md:block"></div>
       <div className="px-4 mx-auto rounded-lg p-4 md:p-2 ">
-        <PorqueNosotros />
+      
       </div>
       <div className=" mx-auto max-w-screen-xl ">
         <CarouselCursosDestacados />
