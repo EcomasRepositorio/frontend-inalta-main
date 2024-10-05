b081ef21064fb66e33d3312e73e2d17457d49907
cristiancutizaca <cutizacamachacacristian@gmail.com>
2024-10-05 18:01:57 -0500

    change background colors in book

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -68,7 +68,7 @@ const Book = () => {
   return (
     <section
       id="contacto"
-      className="bg-gradient-to-b from-blue-400 via-white to-blue-400 p-8 rounded-xl min-h-screen"
+      className="bg-transparent p-8 rounded-xl min-h-screen"
     >
       <div className="max-w-screen-lg mx-auto">
         <Image
--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -9,7 +9,7 @@ import Whatsapp from '@/components/whatsapp/Index';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="font-roboto bg-white">
+    <div className="font-roboto bg-gradient-to-b from-blue-400 via-blue-400 to-blue-600 ">
       <ThemeProvider
         attribute="class"
         defaultTheme="light"
